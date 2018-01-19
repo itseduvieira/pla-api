0c381c733a08a4b5e83d1ab1836f50a78ff3d42d
Eduardo Vieira <eduardofelipevieira@gmail.com>
2018-01-19 18:12:52 -0200

    Commit

--- src/route/v1/report.js
+++ src/route/v1/report.js
@@ -7,39 +7,37 @@ const fs = require('fs')
 
 let fonts = {
     Roboto: {
-        normal: path.join(__dirname, '../../../assets/fonts', '/Roboto-Regular.ttf')
+        normal: path.join(__dirname, '../../../assets/fonts', '/Roboto-Regular.ttf'),
+        bold: path.join(__dirname, '../../../assets/fonts', '/Roboto-Bold.ttf')
     }
 }
 
 let PdfPrinter = require('pdfmake/src/printer')
 let printer = new PdfPrinter(fonts)
 
-let data = {
-    "number": "123",
-    "seller": {
-        "name": "Next Step Webs, Inc.",
-        "road": "12345 Sunny Road",
-        "country": "Sunnyville, TX 12345"
-    },
-    "buyer": {
-        "name": "Acme Corp.",
-        "road": "16 Johnson Road",
-        "country": "Paris, France 8060"
-    },
-    "items": [{
-        "name": "Website design",
-        "price": 300
-    }]
-}
-
 router.get('/report', (req, res) => {
     let dd = {
+        pageSize: 'A4',
+        pageOrientation: 'landscape',
+        pageMargins: [ 40, 60, 40, 60 ],
         content: [
             'First paragraph',
-            'Another paragraph'
+            'Another paragraph',
+            {
+                table: {
+                  headerRows: 1,
+                  widths: [ '*', '*', '*', '*' ],
+                  body: [
+                    [ 'First', 'Second', 'Third', 'The last one' ],
+                    [ 'Value 1', 'Value 2', 'Value 3', 'Value 4' ],
+                    [ { text: 'Bold value', bold: true }, 'Val 2', 'Val 3', 'Val 4' ]
+                  ]
+                }
+            }
         ]
     }
     
+    res.header("Content-Type", "application/pdf")
     let doc = printer.createPdfKitDocument(dd)
     doc.pipe(res)
     doc.end()
