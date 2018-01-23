bc26d0ffca30cdd72df024316c50bd6a84b1b022
Eduardo Vieira <eduardofelipevieira@gmail.com>
2018-01-23 19:09:45 -0200

    Commit

--- src/route/v1/report.js
+++ src/route/v1/report.js
@@ -15,24 +15,31 @@ let fonts = {
 let PdfPrinter = require('pdfmake/src/printer')
 let printer = new PdfPrinter(fonts)
 
-router.get('/report', (req, res) => {
+router.post('/report', (req, res) => {
+    let body = req.body
+
+    console.log(JSON.stringify(body))
+
+    let table = [
+        [ { text: 'Local do Plantão', bold: true }, { text: 'Data de Entrada', bold: true }, { text: 'Data de Saída', bold: true }, { text: 'Total', bold: true }, { text: 'Valor', bold: true } ]
+    ]
+
+    body.data.forEach(line => {
+        table.push(line)
+    })
+
     let dd = {
         pageSize: 'A4',
         pageOrientation: 'landscape',
         pageMargins: [ 40, 60, 40, 60 ],
         content: [
-            'Eduardo Vieira',
-            'Período: 01/01/2017 a 31/01/2017',
+            body.name,
+            `Período: ${body.startDate} a ${body.endDate}`,
             {
                 table: {
                   headerRows: 1,
                   widths: [ '*', '*', '*', '*', '*' ],
-                  body: [
-                    [ { text: 'Local do Plantão', bold: true }, { text: 'Data de Entrada', bold: true }, { text: 'Data de Saída', bold: true }, { text: 'Total', bold: true }, { text: 'Valor', bold: true } ],
-                    [ 'Santa Casa  (São Paulo)', '23/01/2017 22:00', '24/01/2017 00:00', '2', 'R$1.000,00' ],
-                    [ 'Clínica Oftalmo', '23/01/2017 21:00', '24/01/2017 02:00', '5', 'R$5.000,00' ],
-                    [ 'Santa Casa  (Santos)', '23/01/2017 22:00', '24/01/2017 00:00', '2', 'R$5.000,00' ]
-                  ]
+                  body: table
                 }
             }
         ]
