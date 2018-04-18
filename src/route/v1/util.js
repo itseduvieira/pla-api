d18033baaee0b52d4608795bebac73516e0d4c04
Eduardo Vieira <eduardofelipevieira@gmail.com>
2018-04-18 17:37:15 -0300

    Commit

--- src/route/v1/util.js
+++ src/route/v1/util.js
@@ -3,8 +3,21 @@ const router = express.Router()
 const path = require('path')
 const fs = require('fs')
 const debug = require('debug')
+const crypto = require('crypto')
 const SparkPost = require('sparkpost')
 const client = new SparkPost('aa1f1c693c80bde84af4cac8a49a53f4b0aa5fc0')
+
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+let confSchema = new Schema({
+    email: String,
+    timestamp: String,
+    code: String,
+    confirmed: Boolean
+})
+
+let Confirmation = mongoose.model('Confirmation', confSchema)
 
 router.get('/terms', (req, res) => {
     const file = path.join(__dirname, '../../../assets/docs', 'terms.pdf')
@@ -22,10 +35,48 @@ router.get('/terms', (req, res) => {
     stream.pipe(res)
 })
 
-router.post('/confirm', (req, res) => {
-    let body = req.body
+router.get('/confirm', async (req, res) => {
+    if(!req.query.code) {
+        res.status(301).redirect('https://www.plandoc.com.br')
+    } else {
+        const result = await Confirmation.update({ code: req.query.code }, { $set: { confirmed: true }})
 
-    debug(JSON.stringify(body))
+        if(result.n <= 0) {
+            res.status(301).redirect('https://www.plandoc.com.br')
+        } else {
+            res.setHeader('Content-Type', 'text/html')
+            res.status(200)
+                .send(`<html>
+                            <head>
+                                <title>Plandoc | Email Confirmado</title>
+                            </head>
+                        <body>
+                            <h1>Teste</h1>
+                        </body
+                    </html>`)
+        }
+    }
+})
+
+router.post('/confirm', async (req, res) => {
+    const body = req.body
+    const now = Date.now()
+    
+    const source = body.email + now
+
+    const secret = 'pdc-api';
+    const code = crypto.createHmac('sha256', secret)
+                   .update(source)
+                   .digest('hex')
+
+    let confirmation = new Confirmation({
+        email: body.email,
+        timestamp: now,
+        code: code,
+        confirmed: false
+    })
+
+    await confirmation.save()
 
     client.transmissions.send({
         content: {
@@ -34,7 +85,7 @@ router.post('/confirm', (req, res) => {
             html:`<html>
                     <body>
                       <p>Olá, ${body.name} e bem vindo à Plandoc :)</p></br></br>
-                      <p>Vimos aqui que você fez um cadastro e gostaríamos de te agradecer pela preferência.</p></br></br>Para completar o cadastro, por favor <a href="#">neste link</a> para confirmar seu email.</p></br></br>
+                      <p>Vimos aqui que você fez um cadastro e gostaríamos de te agradecer pela preferência.</p></br></br>Para completar o cadastro, por favor clique <a href="http://localhost:3000/v1/confirm?code=${code}">aqui</a> para confirmar seu email.</p></br></br>
                       <p>Obrigado</p><p><b>Equipe Plandoc</b></p>
                     </body>
                   </html>`
@@ -52,7 +103,6 @@ router.post('/confirm', (req, res) => {
       .catch(err => {
         res.status(500)
             .json({
-                error: true,
                 message: err.message
             })
       })
