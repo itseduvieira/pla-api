db649632ac5fd797aca9e19802383132c74ebdf8
Eduardo Vieira <eduardo.vieira@digicon.com.br>
2018-09-12 16:26:53 -0300

    Commit

--- src/route/v1/profile.js
+++ src/route/v1/profile.js
@@ -5,7 +5,7 @@ const router = express.Router()
 const Profile = require('../../model/profile')
 
 router.get('/profile', async (req, res) => {
-    let profile = await Profile.findOne({ id: res.locals.userId })
+    let profile = await Profile.findOne({ userId: res.locals.userId })
 
     profile = profile || { }
 
